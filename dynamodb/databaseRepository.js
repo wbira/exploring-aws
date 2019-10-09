b3fdf57178602f41c5e2e5e040981d5f432e7db2
Waldemar Bira <waldek.bira@gmail.com>
2019-10-09 09:30:56 +0200

    Finished CRUD implementation

--- dynamodb/databaseRepository.js
+++ dynamodb/databaseRepository.js
@@ -1,7 +1,63 @@
 const AWS = require('aws-sdk');
-const dynamoCLient = new AWS.DynamoDB.DocumentClient();
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+const TableName = process.env.TABLE_NAME;
 
-module.exports.saveItem = item => {};
-module.exports.getItem = item => {};
-module.exports.deleteItem = item => {};
-module.exports.updateItem = item => {};
+module.exports.saveItem = async item => {
+  const params = { TableName, Item: item };
+  return dynamoClient
+    .put(params)
+    .promise()
+    .then(response => {
+      console.log('Puted item attrubiutes:', response.Attributes);
+      return item.itemId; // todo should return real value from table
+    });
+};
+
+module.exports.getItem = async itemId => {
+  const params = {
+    Key: {
+      itemId
+    },
+    TableName
+  };
+
+  return dynamoClient
+    .get(params)
+    .promise()
+    .then(response => {
+      if (response.Item) {
+        return response.Item;
+      }
+      throw Error('item not exits');
+    });
+};
+
+module.exports.deleteItem = async itemId => {
+  const params = {
+    Key: {
+      itemId
+    },
+    TableName
+  };
+
+  return dynamoClient.delete(params).promise();
+};
+
+module.exports.updateItem = async (itemId, paramsName, paramsValue) => {
+  const params = {
+    TableName,
+    Key: {
+      itemId
+    },
+    ConditionExpression: 'attribute_exists(itemId)',
+    UpdateExpression: 'set ' + paramsName + ' = :v',
+    ExpressionAttributeValues: {
+      ':v': paramsValue
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+  return dynamoClient
+    .update(params)
+    .promise()
+    .then(response => response.Attributes);
+};
--- dynamodb/handler.js
+++ dynamodb/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const databaseManager = require('./databaseManager');
+const databaseRepository = require('./databaseRepository');
 const uuidv1 = require('uuid/v1');
 
 function createResponse(statusCode, body) {
@@ -10,7 +10,56 @@ function createResponse(statusCode, body) {
   };
 }
 
-module.exports.getItem = async event => {};
-module.exports.saveItem = async event => {};
-module.exports.deleteItem = async event => {};
-module.exports.updateItem = async event => {};
+module.exports.saveItem = async event => {
+  const item = JSON.parse(event.body);
+  item.itemId = uuidv1();
+
+  try {
+    const response = await databaseRepository.saveItem(item);
+    return createResponse(200, response);
+  } catch (err) {
+    console.log('Error during save item: ', err);
+    return createResponse(500, err);
+  }
+};
+
+module.exports.getItem = async event => {
+  const itemId = event.pathParameters.itemId;
+
+  try {
+    const response = await databaseRepository.getItem(itemId);
+    return createResponse(200, response);
+  } catch (err) {
+    console.log('Error during get item: ', err);
+    return createResponse(500, err);
+  }
+};
+
+module.exports.deleteItem = async event => {
+  const itemId = event.pathParameters.itemId;
+
+  try {
+    const response = await databaseRepository.deleteItem(itemId);
+
+    return createResponse(200, `Item ${itemId} was deleted`);
+  } catch (err) {
+    console.log('Error during delete item: ', err);
+    return createResponse(500, err);
+  }
+};
+
+module.exports.updateItem = async event => {
+  const itemId = event.pathParameters.itemId;
+
+  const body = JSON.parse(event.body);
+  const paramName = body.paramName;
+  const paramValue = body.paramValue;
+
+  try {
+    const response = await databaseRepository.updateItem(itemId, paramName, paramValue);
+    return createResponse(200, response);
+  } catch (err) {
+    console.log('err', err);
+    return createResponse(500, err);
+  }
+};
